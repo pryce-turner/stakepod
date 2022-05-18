8dafe5a9fe0d8ae013740614713b6e2531a54625
pryce-turner <pryce.turner@gmail.com>
2022-05-18 23:33:21 +1200

    Finished convenience test deployment script, made formatting more consistent in template

--- test.sh
+++ test.sh
@@ -1,26 +1,20 @@
 set -e
 
 echo "PLAYING KUBE"
+echo ""
 podman play kube stake_pod_config.yml
 
 sleep 3
 
-EXITED=( $(podman ps -a -f pod=stake_pod -f status=exited --noheading | rev | cut -d ' ' -f1 | rev) )
-
-if [[ $EXITED == "" ]]
-then
-  echo "No containers exited after deployment."
+echo "Containers deployed, any failures will print below:"
+for line in $(podman ps -a -f pod=stake_pod -f status=exited --noheading | rev | cut -d ' ' -f1 | rev) ; do
+  echo $line
+  podman logs $line
   echo ""
-else
-  echo "The following containers did not deploy correctly:"
-  for line in $EXITED; do
-    echo $line
-    podman logs $line
-    echo ""
-  done
-fi
+done
 
-# echo "CLEANING UP"
-# podman pod stop stake_pod
-# sleep 2
-# podman pod rm stake_pod
+echo ""
+echo "CLEANING UP"
+podman pod stop stake_pod
+sleep 2
+podman pod rm stake_pod
